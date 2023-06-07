d72ed8ddf76dae711b25e5571e2edd79adaed313
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-07 23:25:57 +0100

    refactor(parts): change the card image to a solid color

---
+++ src/components/BoostBadge.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from 'react-i18next';
+
+interface Props {
+  boost: number;
+}
+
+const BoostBadge = (props: Props) => {
+  const { boost } = props;
+
+  const { t } = useTranslation();
+
+  return (
+    <span className='text-xs font-medium px-2.5 py-0.5 rounded text-amber-950 bg-amber-500 dark:bg-amber-500'>
+      {t('boostPercentage', { boost })}
+    </span>
+  );
+};
+
+export default BoostBadge;
