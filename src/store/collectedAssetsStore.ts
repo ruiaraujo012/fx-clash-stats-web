0d112083149153100a1d4ca5254870cd286cb7e5
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-10 12:32:26 +0100

    feat(store): add collected assets store

---
+++ src/store/collectedAssetsStore.ts
@@ -0,0 +1,95 @@
+import { create } from 'zustand';
+import { createPartsCollectedSlice } from '@/features/parts';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+import { persist } from 'zustand/middleware';
+import type { PartsDataSlice, PartsKeys } from '@/features/parts';
+
+export type CollectedAssetsKeys = PartsKeys;
+
+type AssetsCollectedState = PartsDataSlice;
+
+type AssetsCollectedActions = {
+  updateCollectedAssetLevel: (assetKey: CollectedAssetsKeys, id: number, level: number) => void;
+  updateCollectedAssetCards: (assetKey: CollectedAssetsKeys, id: number, cards: number) => void;
+  increaseCollectedAssetCards: (assetKey: CollectedAssetsKeys, id: number) => void;
+  decreaseCollectedAssetCards: (assetKey: CollectedAssetsKeys, id: number) => void;
+};
+
+export type AssetsCollectedStore = AssetsCollectedState & AssetsCollectedActions;
+
+const useCollectedAssetsStore = create<AssetsCollectedStore>()(
+  persist(
+    (...args) => ({
+      ...createPartsCollectedSlice(...args),
+      decreaseCollectedAssetCards: (assetKey, id) => {
+        const [set] = args;
+
+        return set((state) => {
+          const currentCollectedCards = state[assetKey][id]?.cards ?? 0;
+
+          return {
+            [assetKey]: {
+              ...state[assetKey],
+              [id]: {
+                ...state[assetKey][id],
+                cards: currentCollectedCards > 0 ? currentCollectedCards - 1 : currentCollectedCards,
+              },
+            },
+          };
+        });
+      },
+      increaseCollectedAssetCards: (assetKey, id) => {
+        const [set] = args;
+
+        return set((state) => {
+          const currentCollectedCards = state[assetKey][id]?.cards ?? 0;
+
+          return {
+            [assetKey]: {
+              ...state[assetKey],
+              [id]: {
+                ...state[assetKey][id],
+                cards: currentCollectedCards + 1,
+              },
+            },
+          };
+        });
+      },
+      updateCollectedAssetCards: (assetKey, id, cards) => {
+        const [set] = args;
+
+        return set((state) => ({
+          [assetKey]: {
+            ...state[assetKey],
+            [id]: {
+              ...state[assetKey][id],
+              cards,
+            },
+          },
+        }));
+      },
+      updateCollectedAssetLevel: (assetKey, id, level) => {
+        const [set] = args;
+
+        return set((state) => ({
+          [assetKey]: {
+            ...state[assetKey],
+            [id]: {
+              ...state[assetKey][id],
+              level,
+            },
+          },
+        }));
+      },
+    }),
+    {
+      name: 'fxcs-assets-collected-store-storage',
+    },
+  ),
+);
+
+if (import.meta.env.DEV) {
+  mountStoreDevtool('[FXCS] Assets Collected Store', useCollectedAssetsStore);
+}
+
+export default useCollectedAssetsStore;
