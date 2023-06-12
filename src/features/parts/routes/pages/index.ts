8fcd4f740f440a9ca8a32f8011b13762b2841f9f
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-12 22:35:12 +0100

    feat(parts): add front wings list page

--- src/features/parts/routes/pages/index.ts
+++ src/features/parts/routes/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as BrakesListPage } from './BrakesListPage';
 export { default as GearboxesListPage } from './GearboxesListPage';
 export { default as RearWingsListPage } from './RearWingsListPage';
+export { default as FrontWingsListPage } from './FrontWingsListPage';
