9eacb1ae07118baefdf97dcc9fbbf0361a7ca3ea
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-28 11:19:43 +0100

    fix(calculators): fix missing container on best team once upgraded

--- src/features/calculators/routes/pages/CalculatorPage.tsx
+++ src/features/calculators/routes/pages/CalculatorPage.tsx
@@ -1,14 +1,14 @@
 import { BestTeam, BestTeamOnceUpgraded } from '../../components';
-import { Hr } from '@/components/ui';
+import { Container, Hr } from '@/components/ui';
 
 const CalculatorPage = () => (
-  <>
+  <Container maxWidth='2xl'>
     <BestTeam />
 
     <Hr className='my-10' />
 
     <BestTeamOnceUpgraded />
-  </>
+  </Container>
 );
 
 export default CalculatorPage;
