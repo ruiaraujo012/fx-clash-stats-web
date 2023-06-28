592326388f7c6a3fb7711d9c789752004bca2b6f
Rui <ruiaraujo012@gmail.com>
2023-06-28 11:07:58 +0000

    Updates

--- assets/index-93613e17.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as e}from"./vendor-71757edf.js";import{A as i}from"./AssetListPage-3e04936d.js";import{u as o}from"./useDrivers-b1049fb3.js";import"./formatting-dff3c03c.js";import"./index-84e5e305.js";const a=()=>{const s=o(),{t:r}=t(["drivers"]);return e.jsx(i,{assetKey:"drivers",assets:s,title:r("drivers:driver",{count:2})})},c=a;export{c as DriversListPage};
