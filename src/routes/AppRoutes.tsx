4f67df4990361815905dd44c4258b8b900753dde
Rui Araújo <ruiaraujo012@gmail.com>
2023-06-30 20:55:59 +0100

    ci(logs): add sentry connections

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,9 +1,12 @@
+import * as Sentry from '@sentry/react';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 
 import commonRoutes from '@/routes/commonRoutes';
 
+const sentryCreateHashRouter = Sentry.wrapCreateBrowserRouter(createHashRouter);
+
 const AppRoutes = () => {
-  const router = createHashRouter([...commonRoutes]);
+  const router = sentryCreateHashRouter([...commonRoutes]);
 
   return <RouterProvider router={router} />;
 };
