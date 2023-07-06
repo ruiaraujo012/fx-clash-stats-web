5e18cc139cfe0961486035412dd5879f8a938579
Rui <ruiaraujo012@gmail.com>
2023-07-06 19:18:58 +0000

    Updates

--- assets/index-5582acbb.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as e}from"./vendor-71757edf.js";import{A as i}from"./AssetListPage-39b9c42d.js";import{u as o}from"./useDrivers-b1049fb3.js";import"./formatting-1893dd47.js";import"./index-721225d5.js";const a=()=>{const s=o(),{t:r}=t(["drivers"]);return e.jsx(i,{assetKey:"drivers",assets:s,title:r("drivers:driver",{count:2})})},c=a;export{c as DriversListPage};
